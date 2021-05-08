8d214fa61b8714bb8bd00b8fc60bdda6333422e4
ravikiranmaroju <36310929+ravikiranmaroju@users.noreply.github.com>
2021-05-09 01:13:22 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,3 @@
-sudo rm -rf /var/lib/docker/image/overlay2/imagedb/content/sha256/*
+sudo rm -rf /var/lib/docker/image/overlay2/imagedb/content/sha256/
+sudo mkdir /var/lib/docker/image/overlay2/imagedb/content/sha256
+sudo docker pull tomcat
